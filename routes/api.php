2b5c78466889a8d6e206dcedaee0001e5fb39eab
Lara-Jalloul <laraadjalloul@gmail.com>
2021-12-28 22:50:31 +0200

    fixing

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,5 @@ Route::group(['prefix' => 'admins'], function() {
         Route::post('/logout', [AdminController::class, 'logout']);
         Route::get('/getAverage', [AdminController::class, 'getAverage']);
         Route::get('/filter', [AdminController::class, 'filter']);
-        Route::get('/countUsers', [AdminController::class, 'CountUsers']);
     });
 });
